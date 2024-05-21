afeabeb389bb3e072d3c2ef4e86986303f6b103f
Henry Kosim <henrykosim@gmail.com>
2024-05-21 21:31:54 +0200

    initial commit

---
+++ src/main/java/com/personalprojects/grocerylist/GrocerylistApplication.java
@@ -0,0 +1,13 @@
+package com.personalprojects.grocerylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerylistApplication.class, args);
+	}
+
+}
